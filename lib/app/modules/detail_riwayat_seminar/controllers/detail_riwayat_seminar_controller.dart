6e1eccb48ef090758faa260ede8063cb82d1f033
andrianazz <andrianwahyu41@gmail.com>
2023-09-28 12:53:01 +0700

    fix: pemb/peng cant view riwayat seminar done

--- lib/app/modules/detail_riwayat_seminar/controllers/detail_riwayat_seminar_controller.dart
+++ lib/app/modules/detail_riwayat_seminar/controllers/detail_riwayat_seminar_controller.dart
@@ -10,6 +10,7 @@ import 'package:url_launcher/url_launcher.dart';
 class DetailRiwayatSeminarController extends GetxController {
   Dio dio = Dio();
 
+  RxString idSeminar = ''.obs;
   RxString nama = ''.obs;
   RxString nim = ''.obs;
   RxString seminar = ''.obs;
@@ -17,20 +18,29 @@ class DetailRiwayatSeminarController extends GetxController {
   RxString tanggal = ''.obs;
   RxString waktu = ''.obs;
   RxString lokasi = ''.obs;
+  RxInt status = 0.obs;
   RxString pembimbing1 = ''.obs;
   RxString pembimbing2 = '-'.obs;
+
+  RxString nimPemb1 = ''.obs;
+  RxString nimPemb2 = ''.obs;
+
   RxString penguji1 = ''.obs;
   RxString penguji2 = '-'.obs;
   RxString penguji3 = '-'.obs;
+
+  RxString nimPeng1 = ''.obs;
+  RxString nimPeng2 = ''.obs;
+  RxString nimPeng3 = ''.obs;
+
   RxString judul = ''.obs;
-  RxString status = ''.obs;
 
   late Future<void> detailPenjadwalan;
 
   @override
   void onInit() {
-    detailPenjadwalan = getDetailPenjadwalan();
     super.onInit();
+    detailPenjadwalan = getDetailPenjadwalan();
   }
 
   //get mahasiswa nama with nim
@@ -61,44 +71,68 @@ class DetailRiwayatSeminarController extends GetxController {
     if (Get.arguments is PenjadwalanKp) {
       PenjadwalanKp penjadwalanKp = Get.arguments;
 
+      idSeminar.value = penjadwalanKp.id!.toString();
       nama.value = await getMahasiswaWithNim(penjadwalanKp.mahasiswaNim!);
       nim.value = penjadwalanKp.mahasiswaNim!;
       seminar.value = penjadwalanKp.jenisSeminar!;
       prodi.value = await getProdiWithId(penjadwalanKp.prodiId!.toString());
       tanggal.value = penjadwalanKp.tanggal!;
+
+      status.value = int.parse(penjadwalanKp.statusSeminar!);
+
       waktu.value = penjadwalanKp.waktu!;
       lokasi.value = penjadwalanKp.lokasi!;
       pembimbing1.value = await getDosenWithNip(penjadwalanKp.pembimbingNip!);
       penguji1.value = await getDosenWithNip(penjadwalanKp.pengujiNip!);
+
+      nimPemb1.value = penjadwalanKp.pembimbingNip!;
+      nimPeng1.value = penjadwalanKp.pengujiNip!;
+
       judul.value = penjadwalanKp.judulKp!;
-      status.value = penjadwalanKp.statusSeminar!;
     } else if (Get.arguments is PenjadwalanSempro) {
       PenjadwalanSempro penjadwalanSempro = Get.arguments;
 
+      idSeminar.value = penjadwalanSempro.id!.toString();
       nama.value = await getMahasiswaWithNim(penjadwalanSempro.mahasiswaNim!);
       nim.value = penjadwalanSempro.mahasiswaNim!;
       seminar.value = penjadwalanSempro.jenisSeminar!;
       prodi.value = await getProdiWithId(penjadwalanSempro.prodiId!.toString());
       tanggal.value = penjadwalanSempro.tanggal!;
+      status.value = int.parse(penjadwalanSempro.statusSeminar!);
       waktu.value = penjadwalanSempro.waktu!;
       lokasi.value = penjadwalanSempro.lokasi!;
+      judul.value = penjadwalanSempro.judulProposal!;
+
       pembimbing1.value =
           await getDosenWithNip(penjadwalanSempro.pembimbingsatuNip!);
-      pembimbing2.value =
-          await getDosenWithNip(penjadwalanSempro.pembimbingduaNip!);
+
+      if (penjadwalanSempro.pembimbingduaNip != null) {
+        pembimbing2.value =
+            await getDosenWithNip(penjadwalanSempro.pembimbingduaNip!);
+      }
+
+      nimPemb1.value = penjadwalanSempro.pembimbingsatuNip!;
+      nimPemb2.value = penjadwalanSempro.pembimbingduaNip ?? "";
+
       penguji1.value = await getDosenWithNip(penjadwalanSempro.pengujisatuNip!);
       penguji2.value = await getDosenWithNip(penjadwalanSempro.pengujiduaNip!);
-      penguji3.value = await getDosenWithNip(penjadwalanSempro.pengujitigaNip!);
-      judul.value = penjadwalanSempro.judulProposal!;
-      status.value = penjadwalanSempro.statusSeminar!;
+
+      if (penjadwalanSempro.pengujitigaNip != null) {
+        penguji3.value =
+            await getDosenWithNip(penjadwalanSempro.pengujitigaNip!);
+      }
+      nimPeng1.value = penjadwalanSempro.pengujisatuNip!;
+      nimPeng2.value = penjadwalanSempro.pengujiduaNip!;
+      nimPeng3.value = penjadwalanSempro.pengujitigaNip ?? "";
     } else {
       PenjadwalanSkripsi penjadwalanSkripsi = Get.arguments;
+
+      idSeminar.value = penjadwalanSkripsi.id!.toString();
 
       String penguji1Nama =
           await getDosenWithNip(penjadwalanSkripsi.pengujisatuNip!.toString());
 
       judul.value = penjadwalanSkripsi.judulSkripsi!;
-      status.value = penjadwalanSkripsi.statusSeminar!;
 
       nama.value = await getMahasiswaWithNim(penjadwalanSkripsi.mahasiswaNim!);
       nim.value = penjadwalanSkripsi.mahasiswaNim!;
@@ -106,16 +140,30 @@ class DetailRiwayatSeminarController extends GetxController {
       prodi.value =
           await getProdiWithId(penjadwalanSkripsi.prodiId!.toString());
       tanggal.value = penjadwalanSkripsi.tanggal!;
+      status.value = int.parse(penjadwalanSkripsi.statusSeminar!);
       waktu.value = penjadwalanSkripsi.waktu!;
       lokasi.value = penjadwalanSkripsi.lokasi!;
       pembimbing1.value =
           await getDosenWithNip(penjadwalanSkripsi.pembimbingsatuNip!);
-      pembimbing2.value =
-          await getDosenWithNip(penjadwalanSkripsi.pembimbingduaNip!);
+
+      if (penjadwalanSkripsi.pembimbingduaNip != null) {
+        pembimbing2.value =
+            await getDosenWithNip(penjadwalanSkripsi.pembimbingduaNip!);
+      }
+
+      nimPemb1.value = penjadwalanSkripsi.pembimbingsatuNip!;
+      nimPemb2.value = penjadwalanSkripsi.pembimbingduaNip ?? "";
+
       penguji1.value = penguji1Nama;
       penguji2.value = await getDosenWithNip(penjadwalanSkripsi.pengujiduaNip!);
-      penguji3.value =
-          await getDosenWithNip(penjadwalanSkripsi.pengujitigaNip!);
+      if (penjadwalanSkripsi.pengujitigaNip != null) {
+        penguji3.value =
+            await getDosenWithNip(penjadwalanSkripsi.pengujitigaNip!);
+      }
+
+      nimPeng1.value = penjadwalanSkripsi.pengujisatuNip!;
+      nimPeng2.value = penjadwalanSkripsi.pengujiduaNip!;
+      nimPeng3.value = penjadwalanSkripsi.pengujitigaNip ?? "";
     }
   }
 
